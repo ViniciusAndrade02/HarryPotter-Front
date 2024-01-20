d160c4d258a2eb509b680c1e10671533f1437d86
Vinicius Andrade <Viniciusjandrade08@gmail.com>
2024-01-20 02:13:04 -0300

    terminado a tela de cadastro

--- src/app/crud/cadastro/cadastro.component.ts
+++ src/app/crud/cadastro/cadastro.component.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, } from '@angular/forms';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { Form, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -14,16 +14,17 @@ import { CommonModule } from '@angular/common';
 export class CadastroComponent implements OnInit{
 
   signupForm!:FormGroup;
+  errorSamePassoword:boolean = false
 
-  constructor(private fb:FormBuilder) {}
+  constructor(private fb:FormBuilder,private router:Router) {}
 
   ngOnInit(): void {
     this.signupForm = this.fb.group({
-      name: ["",[Validators.required,Validators.maxLength(20)]],
-      email: ["",Validators.required,Validators.email,Validators.maxLength(50)],
-      password: ["",Validators.required,Validators.maxLength(20)],
-      repetPassword: ["",Validators.required,Validators.maxLength(20)],
-      birthday: ['',Validators.required]
+      name: ["",[Validators.required,Validators.maxLength(40)]],
+      email: ["",[Validators.required,Validators.email,Validators.maxLength(50)]],
+      password: ["",[Validators.required,Validators.maxLength(20)]],
+      repetPassword: ["",[Validators.required,Validators.maxLength(20)]],
+      house: ['',[Validators.required]]
     }) 
   }
 
@@ -31,8 +32,15 @@ export class CadastroComponent implements OnInit{
     return this.signupForm.get(controlName)
   }
 
+  back(){
+    this.router.navigate(['/crud'])
+  }
 
   onSubmit(){
-    console.log('usdfusefhiudsv')
+    if(this.signupForm.value.password !== this.signupForm.value.repetPassword){
+      return this.errorSamePassoword =true
+    }
+    
+    return console.log(this.signupForm.value)
   }
 }
