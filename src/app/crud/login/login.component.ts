bc959c1ebe6dcb987d1449804baa8c89642dcabe
Vinicius Andrade <Viniciusjandrade08@gmail.com>
2024-01-21 02:32:12 -0300

    finished Login

--- src/app/crud/login/login.component.ts
+++ src/app/crud/login/login.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  login!:FormGroup
+
+  constructor(private fb:FormBuilder) {}
+
+  ngOnInit(): void {
+    this.login = this.fb.group({
+      email:["",[Validators.required,Validators.email,Validators.maxLength(50)]],
+      password:["",[Validators.required,Validators.maxLength(20)]]
+    })
+  }
+
+  taked(controlName:string){
+    return this.login.get(controlName)
+  }
+
+  doingLogin(){
+    console.log('jeifhuwfheuf')
+  }
 }
