9fd5d05943f284ebc0df153c024925143b3c2e9d
Vinicius Andrade <Viniciusjandrade08@gmail.com>
2024-02-25 05:33:55 -0300

    bug lateral do login e cadastro

--- src/app/register/crud.routes.ts
+++ src/app/register/crud.routes.ts
@@ -3,9 +3,9 @@ import { CadastroComponent } from './container/cadastro/cadastro.component';
 import { LoginComponent } from './container/login/login.component';
 
 export const loginRouter: Route[] = [
- { path: '', component: LoginComponent },
+ { path: '',title:'Login', component: LoginComponent },
 ];
 
 export const sighUp:Route[] = [
-  {path:'',component: CadastroComponent},
+  {path:'',title:'SighUp',component: CadastroComponent},
 ]
